de6f7069f0285b9c34dd9f7adf533e4800da37db
Ali.Ghodrat <ght.ali@hotmail.com>
2017-03-13 15:44:39 +0100

    Client class initialized

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Calendar;
+
+public class Client {
+
+}
