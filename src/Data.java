bfa182e3efb1853bd8c03fded8fd9138fe5db700
Ali.Ghodrat <ght.ali@hotmail.com>
2017-03-13 15:53:21 +0100

    Message, Server,.. User created in Data

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,9 @@
 
 public class Data {
+	
+	String Message, server, opratorID, itemName, User;
+	int itemNoInput, itemNoStore;
+	String[] Input = new String[10];
+	double tara, netto, bruttoCheck;
 
 }
