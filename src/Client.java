d17e053d2a0fc08f2c9917ffe9a2d9fd6baa3a9a
Ali.Ghodrat <ght.ali@hotmail.com>
2017-03-13 16:23:14 +0100

    RM20 implemented

--- src/Client.java
+++ src/Client.java
@@ -10,8 +10,23 @@ import java.text.DecimalFormat;
 import java.util.Calendar;
 
 public class Client {
-	
+
 	Data data = new Data();
 
 
-}
+	//Weight asking for operator ID and wait for Answer
+	public void flow1(BufferedReader Answer, DataOutputStream Reply)throws IOException{
+		{
+			data.setMessage("Enter ID");
+			Reply.writeBytes("RM20 8 \"" + data.getMessage() + "\" \" \" \"&3\"\r\n");
+			Reply.flush();
+			this.flow2(Answer, Reply);			
+		}
+		
+		//Operator ID typed and save localy
+		public void flow2(){
+			
+		}
+
+
+	}
