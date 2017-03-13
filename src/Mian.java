99ef9e06774724a3c413d88f2742a5733edee091
Ali.Ghodrat <ght.ali@hotmail.com>
2017-03-13 18:24:10 +0100

    Main done
    IP should change before test

--- src/Mian.java
+++ src/Mian.java
@@ -1,8 +1,27 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 
 public class Mian {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception { 
 		// TODO Auto-generated method stub
+		
+		// IP should change with your current IP
+		Client f = new Client();
+		Socket clientSocket = new Socket("IP", 8000);
+		
+		DataOutputStream Reply = 
+				new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader Answer = 
+				new BufferedReader(new
+						InputStreamReader(clientSocket.getInputStream())); 
+		
+		f.flow1(Answer, Reply);
+
+		clientSocket.close();
 
 	}
 
